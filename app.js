6530bd2254a05a39ee5d72f6a955fd13609e6e4d
Shikhar Dubey <shikhar.db@gmail.com>
2020-12-18 12:11:46 +0530

    Crawler

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cheerio = require('cheerio');
+const request = require('request');
+
+const app = express()
+
+const port = 3001;
+
+app.get('/', function(req, res) {
+    res.status(200).send(`Go to ${req.headers.host}/holders/{tokenAddress} to get the wallet holders`)
+})
+
+app.get('/holders/:id', function (req, res) {
+    request({
+        method: 'GET',
+        url: `https://etherscan.io/token/${req.params.id}`
+    }, (err, response, body) => {
+    
+        if (err) return res.status(500).error(err);
+    
+        let $ = cheerio.load(body);
+    
+        let title = $('title');
+
+        let holder = $('#ContentPlaceHolder1_tr_tokenHolders .col-md-8');
+    
+        console.log(title.text());
+
+        res.json(holder.text().substring(1));
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
